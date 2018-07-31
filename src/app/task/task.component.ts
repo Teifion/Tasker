0e9a434e1d2b4df42d94e41ff73324a3c0e70c24
Teifion <sarkalian@gmail.com>
2018-07-31 12:53:09 +0100

    Stub for showing single task

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../task';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { TaskService }  from '../task.service';
 
 @Component({
   selector: 'app-task',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() task: Task;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private location: Location
+  ) { }
+  
   ngOnInit() {
+    this.getTask();
   }
-
+  
+  goBack(): void {
+    this.location.back();
+  }
+  
+  save(): void {
+     this.taskService.updateTask(this.task)
+       .subscribe(() => this.goBack());
+   }
+  
+  getTask(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.taskService.getTask(id)
+      .subscribe(task => this.task = task);
+  }
 }
